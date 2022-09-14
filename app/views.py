7961c4558321225ef6f2d422bcac12cee38766e0
mesut <yildirimesutcw@gmail.com>
2022-09-14 23:02:27 +0300

    views

--- app/views.py
+++ app/views.py
@@ -9,31 +9,22 @@ from django.contrib import messages
 
 
 # def home(request):
-#     coin = request.GET.get("coin_name")
-#     if coin :
-#         url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-#         response = requests.get(url)
-#         if response.ok : 
-#             content = response.json()
-#             u_coin = content["name"]
-#             if Coin.objects.filter(name=u_coin):
-#               messages.warring(request, "Coin already exists!")
-#             else:
-#                 Coin.objects.create(name=u_coin)
-#                 messages.success(request, "Coin created!")  
-
-#         else:
-#             messages.error(request, "There is no coin")
-
 #     url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    
+#     coin = request.GET.get("coin_name")
+#     print(coin)
 
 #     response = requests.get(url)
 #     content = response.json()
+#     pprint(content[0])
 
-#     pprint(content["name"])
-#     # pprint(content[0]["image"])
-#     # pprint(content[0]["market_cap"])
-#     # pprint(content[0]["price_change_24h"])
+#     # for i in content:
+#     #     print(i)
+#     #     if i.name == coin:
+#     #         pass
+#     #     return
+
+
     
 #     context = {
 #         "coin" : coin,
@@ -47,21 +38,20 @@ from django.contrib import messages
 
 
 
+#     # return render(request)
 #     return render(request, "app/home.html", context)
 
 def home(request):
-    # coin = request.GET.get("coin_name")
-    # url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-    # response = requests.get(url)
-    # content = response.json()
+    coin = request.GET.get("coin_name")
+    url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    response = requests.get(url)
+    content = response.json()
+    
 
     for i in content:
-        coin = request.GET.get("coin_name")
-        url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD& order=market_cap_desc&per_page=100&page=1&sparkline=false"
-        response = requests.get(url)
-        content = response.json()
-        if content[i]["name"] == coin:
-            name_c = content[i]["name"]
+        # print(i["name"])
+        if i["name"] == coin:
+            name_c = i["name"]
 
             if Coin.objects.filter(name=name_c):
                 messages.warning(request, 'Coin already exists!')
@@ -70,32 +60,36 @@ def home(request):
                 messages.success(request, 'Coin created!')
 
         else:
-            messages.error(request, 'There is no city!')
+            messages.error(request, 'There is no coin!')
 
 
     coin_data = []
 
     coins = Coin.objects.all()
-
-    for y in coins:
-
-       if y == coin:
-
-           data = {
-           "name":name
-
-           } 
-
-       coin_data.append(data) 
+    
+    
 
 
-         
+    for k in content:
 
+        for y in coins:
+            print(k["name"])
+            # print(y)
+            
+            if y == k["name"]:
+               print("hello")
+               data = {
+                 "name":k["name"],
+                 "image":k["image"]
+                 } 
+               print(data) 
+               coin_data.append(data)
+               
 
     context ={
          "coin_data" : coin_data
     }    
         
-        
+    # print(coin_data)    
     return render(request, "app/test.html", context)
 # https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false
